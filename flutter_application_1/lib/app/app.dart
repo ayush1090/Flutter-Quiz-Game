399ae3dbfd0d9e488b35db47e3dbb93fe38422f3
Ayush Singh <ayushsingh1081@gmail.com>
2023-12-21 15:26:56 +0530

    Initial commit

---
+++ flutter_application_1/lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     title: "Flutter Learn",
+     home : Container(
+       color: Colors.brown,
+     ),
+    );
+  }
+}
